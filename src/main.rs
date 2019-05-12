7bc66d7cef1d1f20135f68319e60f20e63ac9db0
Cameron Pickett <pickett@fb.com>
2019-05-11 19:24:25 -0700

    Make handler respond with dummy smiirl response

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,24 @@
 extern crate lambda_runtime;
 extern crate serde_derive;
-extern crate simple_error;
 
 use lambda_runtime::{error::HandlerError, lambda, Context};
 use serde_derive::{Deserialize, Serialize};
-use simple_error::bail;
 
 #[derive(Deserialize, Clone)]
 struct CustomEvent {
-    first_name: String,
-    last_name: Option<String>,
 }
 
 #[derive(Serialize, Clone)]
 struct CustomOutput {
-    message: String,
+    number: usize,
 }
 
 fn main() {
     lambda!(my_handler);
 }
 
-fn my_handler(e: CustomEvent, _ctx: Context) -> Result<CustomOutput, HandlerError> {
-    if e.first_name.is_empty() {
-        bail!("Empty first name!");
-    }
-
+fn my_handler(_e: CustomEvent, _ctx: Context) -> Result<CustomOutput, HandlerError> {
     Ok(CustomOutput {
-        message: format!("Hello, {}!", e.first_name),
+        number: 1,
     })
 }
