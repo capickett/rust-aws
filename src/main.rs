07802690b08c8a46f9e48b5f22c8e9eb90ffe081
Cameron Pickett <pickett@fb.com>
2019-05-11 21:45:23 -0700

    Add handling for getting and setting the value
    Added a CounterRequest enum to map get and set for the service.
    
    For now, just calling the service with {"number":123} is sufficient to update the value.

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,55 @@
 extern crate lambda_runtime;
 extern crate serde_derive;
 
-use lambda_runtime::{error::HandlerError, lambda, Context};
+use lambda_runtime::{error::HandlerError, lambda, Context, Handler};
 use serde_derive::{Deserialize, Serialize};
+use std::sync::atomic::{AtomicUsize, Ordering};
 
-#[derive(Deserialize, Clone)]
-struct CustomEvent {
+#[derive(Deserialize)]
+#[serde(untagged)]
+enum CounterRequest {
+    Set { number: usize },
+    Get {},
 }
 
 #[derive(Serialize, Clone)]
-struct CustomOutput {
-    number: usize,
+#[serde(untagged)]
+enum CounterResponse {
+    Get { number: usize },
+    Set { number: usize, message: String },
+}
+
+#[derive(Default)]
+struct CounterHandler {
+    number: AtomicUsize,
+}
+
+impl CounterHandler {
+    fn get_counter(&self) -> Result<CounterResponse, HandlerError> {
+        Ok(CounterResponse::Get {
+            number: self.number.load(Ordering::Relaxed),
+        })
+    }
+
+    fn set_counter(&mut self, number: usize) -> Result<CounterResponse, HandlerError> {
+        self.number.store(number, Ordering::Release);
+        Ok(CounterResponse::Set {
+            number: number,
+            message: "Success!".to_string(),
+        })
+    }
+}
+
+impl Handler<CounterRequest, CounterResponse, HandlerError> for CounterHandler {
+    fn run(&mut self, req: CounterRequest, _ctx: Context) -> Result<CounterResponse, HandlerError> {
+        match req {
+            CounterRequest::Get {} => self.get_counter(),
+            CounterRequest::Set { number: num } => self.set_counter(num),
+        }
+    }
 }
 
 fn main() {
-    lambda!(my_handler);
-}
-
-fn my_handler(_e: CustomEvent, _ctx: Context) -> Result<CustomOutput, HandlerError> {
-    Ok(CustomOutput {
-        number: 1,
-    })
+    let handler = CounterHandler::default();
+    lambda!(handler);
 }
